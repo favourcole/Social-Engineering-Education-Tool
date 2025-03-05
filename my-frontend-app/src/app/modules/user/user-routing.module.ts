fa7cc7ffecc69956794bf3700b8fd2a1972af459
favourcole <favourcole2003@gmail.com>
2025-03-05 03:32:52 +0000

    feat: configure routes for user and login components

--- my-frontend-app/src/app/modules/user/user-routing.module.ts
+++ my-frontend-app/src/app/modules/user/user-routing.module.ts
@@ -3,18 +3,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from '../../components/user/user.component';
+import { LoginComponent } from '../../components/login/login.component';
 import { DashboardComponent } from '../../components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {
-    path: 'user',
-    component: UserComponent
-  },
-
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
+  { path: 'user', component: UserComponent },
+  { path: 'login', component: LoginComponent },
+  // Add other routes as needed
 ];
 
 @NgModule({
