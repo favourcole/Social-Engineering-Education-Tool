54e003d8c47572262b3e39fa91044f20c1ca36c6
favourcole <favourcole2003@gmail.com>
2025-03-05 03:26:59 +0000

    feat: implement reactive form controls with validation in login component

--- my-frontend-app/src/app/components/login/login.component.ts
+++ my-frontend-app/src/app/components/login/login.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  // Initialize loginForm here
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 
+  ngOnInit(): void {
+    // Initialization moved to the property declaration
+  }
+
+  onSubmit(): void {
+    console.log(this.loginForm.value);
+  }
 }
