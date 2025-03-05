95d0a313d55c1e54c247452239f768772ab716b4
favourcole <favourcole2003@gmail.com>
2025-03-05 03:40:04 +0000

    feat: convert user component to standalone with proper module imports

--- my-frontend-app/src/app/components/user/user.component.ts
+++ my-frontend-app/src/app/components/user/user.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
+  imports: [CommonModule, FormsModule],
+  standalone: true
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   username: string = '';
   submitted: boolean = false;
+  userData: any = {};  // Initialize userData
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
   submitUserForm() {
     this.submitted = true;
+    // You can add additional logic here if needed
+    // For example, saving the username to userData
+    this.userData.name = this.username;
   }
 }
