b0849741859da7b68a729f389800b31a73238b53
favourcole <favourcole2003@gmail.com>
2025-03-07 22:33:55 +0000

    fix: ensure login route is properly configured in routes

--- my-frontend-app/src/app/app.routes.ts
+++ my-frontend-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { UserComponent } from './components/user/user.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'user', component: UserComponent },
-  { path: '', component: HomeComponent, pathMatch: 'full' }, // Default home route
-  { path: '**', redirectTo: '' } // Redirects any unknown path to home
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '' }
 ];
