ac789d9b28c880ecddebd64a19f59f805351debd
favourcole <favourcole2003@gmail.com>
2025-03-08 03:08:54 +0000

    feat: add logout functionality to app component

--- my-frontend-app/src/app/app.component.ts
+++ my-frontend-app/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterModule]
+  imports: [RouterModule, CommonModule]
 })
 export class AppComponent {
   title = 'angular-app';
+  
+  constructor(public authService: AuthService) {}
+  
+  logout(): void {
+    this.authService.logout();
+  }
 }
