6766b126ca573198fc00ef3a4627fbbef2b2d4ec
favourcole <favourcole2003@gmail.com>
2025-03-08 03:08:32 +0000

    feat: enhance login component with authentication functionality

--- my-frontend-app/src/app/components/login/login.component.ts
+++ my-frontend-app/src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,17 +12,29 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule, ReactiveFormsModule]
 })
 export class LoginComponent implements OnInit {
-  // Initialize loginForm here
   loginForm: FormGroup = new FormGroup({
     username: new FormControl('', [Validators.required, Validators.minLength(3)]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)])
   });
+  
+  submitted = false; // Add this property
+  
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     // Initialization moved to the property declaration
   }
 
   onSubmit(): void {
-    console.log(this.loginForm.value);
+    this.submitted = true; // Set to true when form is submitted
+    
+    if (this.loginForm.valid) {
+      const username = this.loginForm.get('username')?.value;
+      const password = this.loginForm.get('password')?.value;
+      
+      if (this.authService.login(username, password)) {
+        this.router.navigate(['/user']);
+      }
+    }
   }
 }
